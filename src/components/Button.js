9dc117225d951b113d2805e8130807c02027ae1b
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-12-19 14:50:00 -0500

    👍 Fix: Button.js code style (My prettier isn't working)

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,5 +4,10 @@ import "components/Button.scss";
 import classNames from "classnames";
 
 export default function Button(props) {
-  return <button className={classNames('button', { "button--confirm": props.confirm, "button--danger": props.danger } )} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
+  return <button
+    className={classNames('button', { "button--confirm": props.confirm, "button--danger": props.danger } )}
+    onClick={props.onClick}
+    disabled={props.disabled}>
+      {props.children}
+    </button>;
 }
